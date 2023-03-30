1935888ed9d9e3050e52286246d3ed1d5eb166a7
faz-cxr <faz.icxr@gmail.com>
2023-03-31 03:39:28 +0400

    Improve Prompts and sources

--- query_data.py
+++ query_data.py
@@ -2,12 +2,16 @@
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
 from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
-from langchain.chains.question_answering.stuff_prompt import PROMPT_SELECTOR
 from langchain.chains.llm import LLMChain
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.chat_models import PromptLayerChatOpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores.base import VectorStore
+
+import promptlayer
+promptlayer.api_key = "pl_2b1769e7202b6a141d4491fca41e308a"
 
 
 def get_chain(
@@ -26,24 +30,57 @@ def get_chain(
         question_manager.add_handler(tracer)
         stream_manager.add_handler(tracer)
 
-    question_gen_llm = ChatOpenAI(
+    question_gen_llm = PromptLayerChatOpenAI(
+        pl_tags=["chat-nice-ws-qgen"],
         temperature=0,
         verbose=True,
         callback_manager=question_manager,
     )
-    streaming_llm = ChatOpenAI(
+    streaming_llm = PromptLayerChatOpenAI(
+        pl_tags=["chat-nice-ws-stream"],
         streaming=True,
         callback_manager=stream_manager,
         verbose=True,
         temperature=0,
     )
+    template = """You are given the following extracted parts of a long document and a question, provide a conversational answer.
+If the question includes a request for code, provide a code block directly from the documentation.
+If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+If the question can't be answered from the extracted parts, politely inform them that you are tuned to only answer questions about NICE guidelines fed to you.
 
+ALWAYS return a "SOURCES" part in your answer.
+The "SOURCES" part should be hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
+Example of your response should be:
+
+```
+The answer is foo
+
+SOURCES: https://cks.nice.org.uk/xyz
+
+```
+
+# Extracted documents:
+---
+{summaries}
+---
+
+Question: {question}"""
+    QA_PROMPT = PromptTemplate(template=template, input_variables=["question", "summaries"])
+    _template = """Given the following conversation and a follow up question. Frame a standalone question combining all information from the Chat History with the Follow Up Input.
+Please ignore the SOURCES.
+Chat History:
+---
+{chat_history}
+---
+Follow Up Input: {question}
+Standalone question:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
     question_generator = LLMChain(
         llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
     )
-    qa_prompt = PROMPT_SELECTOR.get_prompt(streaming_llm)
-    doc_chain = load_qa_chain(
-        streaming_llm, chain_type="stuff", prompt=qa_prompt, callback_manager=manager
+
+    doc_chain = load_qa_with_sources_chain(
+        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
 
     qa = ConversationalRetrievalChain(
@@ -51,6 +88,5 @@ def get_chain(
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
         callback_manager=manager,
-        return_source_documents=True
     )
     return qa
